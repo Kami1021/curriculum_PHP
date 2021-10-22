b63e3f5a1bcb535cda8b90195bab737d272d7eb5
Tomoya <tomoya10632140@gmail.com>
2021-10-22 20:19:33 +0900

    課題提出_2-11
    index.php
    result.php
    の作成

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$mail = $_POST['mail'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
